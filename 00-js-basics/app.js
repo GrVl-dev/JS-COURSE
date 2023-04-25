d1dae881b50139dd870a9a5df58ed440854fb1ef
Gryuger Vladimir <vlgrv80@gmail.com>
2023-04-25 21:23:00 +0400

    завершил модуль'

--- 00-js-basics/app.js
+++ 00-js-basics/app.js
@@ -1,4 +1,23 @@
-let x = 1
-let y = 1
-console.log(x++)
-console.log(++y)
+let secretNum = Math.ceil(Math.random() * 10)
+let tries = 0
+
+function guessNum(num) {
+  tries++
+  if (num === secretNum) {
+    alert('Ура, Вы угадали число!')
+    secretNum = Math.ceil(Math.random() * 10)
+    tries = 0
+  } else {
+    if (num > secretNum) {
+      alert('Неверно, ваше число больше, попробуйте еще!')
+    } else {
+      alert('Неверно, ваше число меньше, попробуйте еще!')
+    }
+    
+  }
+  if (tries === 3){
+    alert('Вы исчерпали лимит попыток')
+    secretNum = Math.ceil(Math.random() * 10)
+    tries = 0
+  }
+}
